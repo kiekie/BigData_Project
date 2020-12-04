000f7f3865a47ee35c5ba0a19d0a41ade1449886
Burn7elder <44440430+Burn7elder@users.noreply.github.com>
2020-12-04 21:38:23 +0800

    Add files via upload
    #ad.csv
    csv_path1=sys.argv[1]
    #click_log.csv
    csv_path2=sys.argv[2]
    
    #只输入txt文件名即可，不用增加后缀
    txtpath=sys.argv[3]
    
    #输入categroy，creative_id，industry其中之一，选择需要读取得到的数据。
    #针对kmeans 不要写入txt后再使用数据，会遗失点击次数的数据
    readtype=sys.arg[4]
    eg  根据类目id生成文件
    python gtxt.py ad.csv click_log.csv gtxt categroy

--- freq_Ren/gtxt.py
+++ freq_Ren/gtxt.py
@@ -15,11 +15,16 @@ user_click={}
 csv_path1=sys.argv[1]
 #click_log.csv
 csv_path2=sys.argv[2]
+
+#只输入txt文件名即可，不用增加后缀
 txtpath=sys.argv[3]
+
+#输入categroy，creative_id，industry其中之一，选择需要读取得到的数据。
+#针对kmeans 不要写入txt后再使用数据，会遗失点击次数的数据
 readtype=sys.arg[4]
 
 #csv_path1='adtest.csv'
-#csv_path2='fretest.csv'
+#csv_path2='click_log.csv'
 #txtpath='gtxt'
 #readtype='creative_id'
 #readtype='categroy'
@@ -27,6 +32,7 @@ readtype=sys.arg[4]
 
     
 def data_creative(csv_path,txtpath):
+#    每一行为用户点击的广告素材id，针对频繁项集，每一行第一列为用户id
     with open(csv_path,'r',encoding='utf-8') as csvfile:
       reader = csv.DictReader(csvfile)
       for row in reader:
@@ -37,12 +43,12 @@ def data_creative(csv_path,txtpath):
           else:
               user_click[int(row['user_id'])].add(row['creative_id'])
     csvfile.close()
-
+    print(user_click)
     if os.path.exists(txtpath+"_creative.txt"):
         os.remove(txtpath+"_creative.txt")
     with open(txtpath+"_creative.txt", 'a') as f:
         for keys,items in user_click.items():
-            str1=''
+            str1=str(keys)+" "
             for item in items:
                 str1=str1+item+' '
             f.write(str1)
@@ -85,6 +91,7 @@ def data_categroy(csv_path1,csv_path2,txtpath):
                           user_click[int(row['user_id'])][categroy]+=int(row['click_times'])
     csvfile.close()
 #    print(user_click)
+    #    写入文件未保存点击次数数据
     if os.path.exists(txtpath+'_categroy.txt'):
         os.remove(txtpath+'_categroy.txt')
     with open(txtpath+'_categroy.txt','a') as f:
@@ -135,7 +142,7 @@ def data_industryid(csv_path1,csv_path3,txtpath):
     csvfile.close()
     
 #    print(user_click)
-    
+#    写入文件未保存点击次数数据
     if os.path.exists(txtpath+'_industry.txt'):
         os.remove(txtpath+'_industry.txt')
     with open(txtpath+'_industry.txt','a') as f:
@@ -156,13 +163,13 @@ elif readtype=="industry":
     data_industryid(csv_path1,csv_path2,txtpath)
 
 #原用户点击数据
-#original_list=[]    
-#with open(txtpath+"_industry.txt", 'r') as file:
-#    for line in file:
-#        s = line.strip().split(' ')
-#        tarray=np.array(s)
-#        original_list.append(tarray.astype(np.int).tolist())
-#print("original_list",original_list)
+original_list=[]    
+with open(txtpath+"_creative.txt", 'r') as file:
+    for line in file:
+        s = line.strip().split(' ')
+        tarray=np.array(s)
+        original_list.append(tarray.astype(np.int).tolist())
+print("original_list",original_list)
     
     
     
