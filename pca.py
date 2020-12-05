5b0b6c36b64622d1d894f936a8a4dedf9e208bec
Song Yanting <ysong408@gatech.edu>
2020-12-05 23:24:10 +0800

    测试了k_means的训练部分

--- pca.py
+++ pca.py
@@ -5,11 +5,11 @@ Created on Tue Nov 24 16:24:22 2020
 @author: Bingo
 """
 
-import gtxt
 import csv
 import os
-from numpy import linalg as la
+import sys
 import numpy as np
+import numpy.linalg as la
 import collections
 
 from sklearn.cluster import KMeans
@@ -17,96 +17,92 @@ from sklearn.decomposition import PCA
 from sklearn.metrics import silhouette_score, adjusted_rand_score
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import LabelEncoder, MinMaxScaler
-from sklearn.externals import joblib
+import joblib
 
 
 ADTEST_PATH = 'G:/BDT5741/BDTproject/adtest.csv'
 FRETEST_PATH = 'G:/BDT5741/BDTproject/fretest.csv'
-KMEANS_MODEL_PATH= './model/doc_cluster.pkl'
+KMEANS_MODEL_PATH= './BigData_Project/test/doc_cluster.pkl'
+USER_INDUSTRY_PATH = "./BigData_Project/test/industry_industry.txt"
 
+####################### 准备数据 begin ###########################################
 #key为类目id，value为素材id
 product_categroy={}
-with open(ADTEST_PATH,'r',encoding='utf-8') as csvfile:
-    reader = csv.DictReader(csvfile)
-    for row in reader:
-        if int(row['product_category']) not in product_categroy:
-            product_categroy[int(row['product_category'])]=set()
-            if row['creative_id'] not in product_categroy[int(row['product_category'])]:  
+def produca_data():
+    with open(ADTEST_PATH,'r',encoding='utf-8') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            if int(row['product_category']) not in product_categroy:
+                product_categroy[int(row['product_category'])]=set()
+                if row['creative_id'] not in product_categroy[int(row['product_category'])]:  
+                    product_categroy[int(row['product_category'])].add(row['creative_id'])
+            else:
                 product_categroy[int(row['product_category'])].add(row['creative_id'])
-        else:
-            product_categroy[int(row['product_category'])].add(row['creative_id'])
-csvfile.close()  
-print(product_categroy)
+    csvfile.close()  
+    print(product_categroy)
 
-user_click={}
-with open(FRETEST_PATH,'r',encoding='utf-8') as csvfile:
-    reader = csv.DictReader(csvfile)
-    for row in reader:
-        if int(row['user_id']) not in user_click:
-            user_click[int(row['user_id'])]={}
-            for categroy in product_categroy.keys():
-                if row['creative_id'] in product_categroy[categroy]:
-                    if categroy not in user_click[int(row['user_id'])]:
-                        user_click[int(row['user_id'])][categroy]=1
-                    else:
-                        user_click[int(row['user_id'])][categroy]+=1
-        else:
-            for categroy in product_categroy.keys():
-                if row['creative_id'] in product_categroy[categroy]:
-                    if categroy not in user_click[int(row['user_id'])]:
-                        user_click[int(row['user_id'])][categroy]=1
-                    else:
-                        user_click[int(row['user_id'])][categroy]+=1
-csvfile.close()
-
-#字典嵌入字典  key1为用户id，内层字典key为广告类目,value为观看次数
-print(user_click)
-
-
-def user_industry():
-    user_industry_click={}
-
+    user_click={}
     with open(FRETEST_PATH,'r',encoding='utf-8') as csvfile:
         reader = csv.DictReader(csvfile)
         for row in reader:
-            if int(row['user_id']) not in user_industry_click:
-                user_industry_click[int(row['user_id'])]={}
+            if int(row['user_id']) not in user_click:
+                user_click[int(row['user_id'])]={}
                 for categroy in product_categroy.keys():
                     if row['creative_id'] in product_categroy[categroy]:
-                        if categroy not in user_industry_click[int(row['user_id'])]:
-                            user_industry_click[int(row['user_id'])][categroy]=int(row['click_times'])
+                        if categroy not in user_click[int(row['user_id'])]:
+                            user_click[int(row['user_id'])][categroy]=1
                         else:
-                            user_industry_click[int(row['user_id'])][categroy]+=int(row['click_times'])
+                            user_click[int(row['user_id'])][categroy]+=1
             else:
                 for categroy in product_categroy.keys():
                     if row['creative_id'] in product_categroy[categroy]:
-                        if categroy not in user_industry_click[int(row['user_id'])]:
-                            user_industry_click[int(row['user_id'])][categroy]=int(row['click_times'])
+                        if categroy not in user_click[int(row['user_id'])]:
+                            user_click[int(row['user_id'])][categroy]=1
                         else:
-                            user_industry_click[int(row['user_id'])][categroy]+=int(row['click_times'])
+                            user_click[int(row['user_id'])][categroy]+=1
     csvfile.close()
 
-    return user_industry_click
+    #字典嵌入字典  key1为用户id，内层字典key为广告类目,value为观看次数
+    print(user_click)
+####################### 准备数据 end ###########################################
+
 
 # reuse user_click and compute matrix
 ####################### k_means begin ###########################################
 CATEGORY = 32
 N_CLUSTERS = 10
 
+"""
 matrix = [[0 for _ in range(CATEGORY)] for _ in range(len(user_click.keys()))]
-
-def user_category_matrix():
+def read_data():
     for user, categroies in user_click:
         for categroy, click in categroies:
             matrix[user][categroy] = click
+"""
 
+def user_industry():
+    user_industry_click=[]
+
+    with open(USER_INDUSTRY_PATH,'r',encoding='utf-8') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            user = [0] * 336
+            for i_d in range(336):
+                industry = "industry_" + str(i_d)
+                user[i_d] = int(row[industry])
+            user_industry_click.append(user)
+    csvfile.close()
+    print("user_industry_click", len(user_industry_click))
+    return user_industry_click
+
+## call this only at the begining, once k-means are done. All labels are preserved in KMEANS_MODEL_PATH
 def k_means():
     """
     We only need to do k_means once and checkpoint lables
     Do k_means occasionally in need.
     TODO: replace Kmeans with minibatch-kmeans?
     """
-    user_category_matrix(matrix)
+    data = user_industry()
 
     clusterer = Pipeline (
         [
@@ -116,24 +112,25 @@ def k_means():
                 KMeans(
                     n_clusters=N_CLUSTERS,
                     init="k-means++",
-                    n_init=50,
+                    n_init=40,
                     max_iter=500,
                     random_state=42,
                 ),
             ),
         ]
     )
-    clusterer.fit(matrix)
+    clusterer.fit(data)
 
     ## get users in the same cluster
     clf = clusterer[1]
-    print(clf.labels_)
     clusters = clf.labels_.tolist()
-    print(clusters)
+    print(clusters)   ## TODO: remove me 
 
-    ## store our cluster model
+    ## store cluster models
     joblib.dump(clf,  KMEANS_MODEL_PATH)
 
+
+## When only need to call find_neighbors when doing query
 def find_neighbors(user_id):
     """
     First we load model we trained before in K-means
@@ -147,29 +144,19 @@ def find_neighbors(user_id):
     return neighbors
 
 def recommend(user_id):
+    neighbors = find_neighbors(user_id)
+    cloest_neighors = []
+    # TODO: get top 10 cloest neighbors
+
+    # TODO: get creative ids from top 10 cloest neighbors in the same cluster
     pass
-    
-## Then we do cluster on those neighbors based on industry id
 
 ####################### k_means end ###########################################
 
 
+
 ####################### pca begin #############################################
-tryd=[]
-testlist1=[0]*5
-testlist2=[0]*5
-testlist3=[0]*5
-testlist1[0]=1
-testlist1[4]=25
-for key in product_categroy.keys():
-    testlist2[key-1]=key+3 
-for key in product_categroy.keys():
-    testlist3[key-1]=key*2-1 
-tryd.append(testlist1)
-tryd.append(testlist2)
-tryd.append(testlist3)
-tryd=np.array(tryd)
-print(tryd)
+
 def pca(data_mat, top_n_feat=1):
     """ 
     主成分分析：  
@@ -177,7 +164,6 @@ def pca(data_mat, top_n_feat=1):
          保留前n个特征top_n_feat，默认全保留
     返回：降维后的数据集和原始数据被重构后的矩阵（即降维后反变换回矩阵）
     """  
-
     # 获取数据条数和每条的维数 
     print("data_mat",type(data_mat),data_mat.dtype)
     num_data,dim = data_mat.shape  
@@ -208,14 +194,49 @@ def pca(data_mat, top_n_feat=1):
     low_d_data_mat = mean_removed * reg_eig_vects # shape: (100, top_n_feat), top_n_feat最大为特征总数
     recon_mat = ((low_d_data_mat * reg_eig_vects.T) + mean_vals).astype(np.uint8) # 根据前几个特征向量重构回去的矩阵，shape:(100, 784)
 
-#    print("recon_mat",recon_mat.dtype)
-#    recon_mat.dtype="float64"
-#    print("recon_mat",recon_mat.shape)
-    
+    #    print("recon_mat",recon_mat.dtype)
+    #    recon_mat.dtype="float64"
+    #    print("recon_mat",recon_mat.shape)
     return low_d_data_mat, recon_mat
 
 
+def pca_test():
+    tryd=[]
+    testlist1=[0]*5
+    testlist2=[0]*5
+    testlist3=[0]*5
+    testlist1[0]=1
+    testlist1[4]=25
+    for key in product_categroy.keys():
+        testlist2[key-1]=key+3 
+    for key in product_categroy.keys():
+        testlist3[key-1]=key*2-1 
+    tryd.append(testlist1)
+    tryd.append(testlist2)
+    tryd.append(testlist3)
+    tryd=np.array(tryd)
+    print(tryd)
+    low_d_data_mat, recon_mat=pca(tryd)
+    print(recon_mat)
+
+####################### pca end #############################################
 
 
-low_d_data_mat, recon_mat=pca(tryd)
-print(recon_mat)
+def main():
+    # train k_means
+    if sys.argv[1] == "train":
+        k_means()
+    
+    # query k_means
+    """
+    if sys.argv[1] == "query":
+        if not sys.argv[2].isdigit():
+            print("User id should be a digit")
+            exit
+        recommend(int(sys.argv[2]))
+    """
+
+    ## TODO: PCA part
+
+if __name__ == "__main__":
+    main()
